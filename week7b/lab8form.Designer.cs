129677f6be09ce38d88bc09415f7bfd93610c844
300903179 <300903179@PR-E216-42>
2019-06-26 15:27:33 -0400

    added title lable

--- week7b/lab8form.Designer.cs
+++ week7b/lab8form.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TitleLable = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // TitleLable
+            // 
+            this.TitleLable.AutoSize = true;
+            this.TitleLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLable.Location = new System.Drawing.Point(433, 151);
+            this.TitleLable.Name = "TitleLable";
+            this.TitleLable.Size = new System.Drawing.Size(96, 37);
+            this.TitleLable.TabIndex = 0;
+            this.TitleLable.Text = "lab08";
             // 
             // lab8form
             // 
@@ -36,14 +47,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.TitleLable);
             this.Name = "lab8form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "lab";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TitleLable;
     }
 }
 
